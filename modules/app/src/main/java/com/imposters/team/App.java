668d738716bf1b4dac9317842d56df9aa5f4a338
Cabbar Serif <cabbar.serif@hotmail.com>
2021-04-18 02:20:38 +0300

    Login page effects added

--- modules/app/src/main/java/com/imposters/team/App.java
+++ modules/app/src/main/java/com/imposters/team/App.java
@@ -2,6 +2,7 @@ package com.imposters.team;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -12,14 +13,25 @@ import javafx.stage.StageStyle;
  *
  */
 public class App extends Application{
+    protected static Parent root;
+    protected static Stage primaryStageOfprogram;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/loginViews/login.fxml"));
+        this.root = FXMLLoader.load(getClass().getResource("/loginViews/login.fxml"));
         primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(new Scene(this.root));
         primaryStage.setResizable(false);
+        primaryStageOfprogram = primaryStage;
         primaryStage.show();
+    }
+
+    public static Parent getRoot(){
+        return root;
+    }
+    public static Stage getPrimaryStageOfprogram(){ return primaryStageOfprogram;}
+    public static void setCursor(Cursor style){
+        root.setCursor(style);
     }
 
     public static void main( String[] args )
--- modules/app/src/main/java/com/imposters/team/controllers/loginViews/LoginController.java
+++ modules/app/src/main/java/com/imposters/team/controllers/loginViews/LoginController.java
@@ -1,16 +1,29 @@
 package com.imposters.team.controllers.loginViews;
+import com.imposters.team.App;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.Cursor;
+import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.effect.InnerShadow;
+import javafx.scene.paint.Color;
+
+import java.security.Guard;
+
 
 public class LoginController {
 
     @FXML
     private Button loginBtn;
+
+    @FXML
+    private Label closeBtn;
 
     @FXML
     private Label textField;
@@ -25,7 +38,7 @@ public class LoginController {
     private CheckBox manuelInputCheckBox;
 
     @FXML
-    public void loginBtnClicked(ActionEvent event){
+    public void loginBtnClicked(ActionEvent event) {
         String password = passwordTextField.getText();
         String username = usernameTextField.getText();
         if(!(password.isEmpty() || username.isEmpty())){
@@ -41,4 +54,43 @@ public class LoginController {
         }
     }
 
+    @FXML
+    public void onMouseEntered(){
+        App.setCursor(Cursor.HAND);
+        loginBtn.setStyle(
+//                "-fx-border-color: #9F1010;" +
+//                "-fx-text-fill: #9F1010;" +
+                "-fx-background-color: #DEDEDE;" +
+                "-fx-fill: true");
+    }
+
+    @FXML
+    public void onMouseExited(){
+        App.setCursor(Cursor.DEFAULT);
+        loginBtn.setStyle("-fx-border-color: #054671;-fx-text-fill: #054671;-fx-background-color: #ffffff");
+    }
+
+    @FXML
+    public void onCloseClicked(){
+        App.getPrimaryStageOfprogram().close();
+    }
+
+    @FXML
+    public void onCloseEntered(){
+        App.setCursor(Cursor.HAND);
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setRadius(5.0);
+        dropShadow.setOffsetX(3.0);
+        dropShadow.setOffsetY(3.0);
+        dropShadow.setColor(Color.color(0.2, 0.5, 0.5));
+        closeBtn.setEffect(dropShadow);
+        closeBtn.setStyle("-fx-text-fill: #c51300");
+    }
+
+    @FXML
+    public void onCloseExited(){
+        App.setCursor(Cursor.DEFAULT);
+        closeBtn.setEffect(null);
+        closeBtn.setStyle("-fx-text-fill: #2f2f2f");
+    }
 }
