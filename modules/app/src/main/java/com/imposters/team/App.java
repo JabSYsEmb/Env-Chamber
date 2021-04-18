7bcc23c83b9f9bbe66554e3c0db102f5790f2b17
Cabbar Serif <cabbar.serif@hotmail.com>
2021-04-18 13:36:33 +0300

    Add css styelsheet and fix some mess with hovering and entering functions of buttons

--- modules/app/src/main/java/com/imposters/team/App.java
+++ modules/app/src/main/java/com/imposters/team/App.java
@@ -1,40 +1,32 @@
 package com.imposters.team;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Cursor;
-import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.StageStyle;
+import javafx.application.Application;
 
 /**
  * Hello world!
  *
  */
 public class App extends Application{
-    protected static Parent root;
     protected static Stage primaryStageOfProgram;
     private String css;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        this.root = FXMLLoader.load(getClass().getResource("/loginViews/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/loginViews/login.fxml"));
         primaryStage.initStyle(StageStyle.UNDECORATED);
         css = App.class.getResource("/style/style.css").toExternalForm();
         root.getStylesheets().add(css);
-        primaryStage.setScene(new Scene(this.root));
+        primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStageOfProgram = primaryStage;
         primaryStage.show();
     }
 
-    public static Parent getRoot(){
-        return root;
-    }
     public static Stage getPrimaryStageOfProgram(){ return primaryStageOfProgram;}
-    public static void setCursor(Cursor style){
-        root.setCursor(style);
-    }
 
     public static void main( String[] args )
     {
--- modules/app/src/main/java/com/imposters/team/controllers/loginViews/LoginController.java
+++ modules/app/src/main/java/com/imposters/team/controllers/loginViews/LoginController.java
@@ -3,7 +3,6 @@ import com.imposters.team.App;
 
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
-import javafx.scene.Cursor;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -13,13 +12,7 @@ import javafx.scene.control.TextField;
 public class LoginController {
 
     @FXML
-    private Button loginBtn;
-
-    @FXML
-    private Label closeBtn;
-
-    @FXML
-    private Label textField;
+    private Label alertMessage;
 
     @FXML
     private TextField passwordTextField;
@@ -28,14 +21,11 @@ public class LoginController {
     private TextField usernameTextField;
 
     @FXML
-    private CheckBox manuelInputCheckBox;
-
-    @FXML
     public void loginBtnClicked(ActionEvent event) {
         String password = passwordTextField.getText();
         String username = usernameTextField.getText();
         if(!(password.isEmpty() || username.isEmpty())){
-            textField.setText(
+            alertMessage.setText(
                     "logged in successfully, " +
                     Character.toUpperCase(username.charAt(0)) +
                     username.substring(1) + "!"
@@ -43,51 +33,13 @@ public class LoginController {
         }else{
             passwordTextField.clear();
             usernameTextField.clear();
-            textField.setText("invalid username or password, try again!");
+            alertMessage.setText("invalid username or password, try again!");
         }
-    }
-
-    @FXML
-    public void onMouseEntered(){
-        App.setCursor(Cursor.HAND);
-            loginBtn.setStyle(getBtnEnteredMode());
-    }
-
-    @FXML
-    public void onMouseExited(){
-        App.setCursor(Cursor.DEFAULT);
-        loginBtn.setStyle(null);
     }
 
     @FXML
     public void onCloseClicked(){
         App.getPrimaryStageOfProgram().close();
-    }
-
-    @FXML
-    public void onCloseEntered(){
-        App.setCursor(Cursor.HAND);
-        closeBtn.setStyle(getCloseEnteredMode());
-    }
-
-    @FXML
-    public void onCloseExited(){
-        App.setCursor(Cursor.DEFAULT);
-        closeBtn.setEffect(null);
-        closeBtn.setStyle("-fx-text-fill: #2f2f2f");
-    }
-
-    public String getBtnEnteredMode(){
-        return  "-fx-effect:             dropshadow(three-pass-box," +
-                                        "#8195ad, 1, 0.5, 3, 2);" +
-                "-fx-text-fill:          #ffffff;" +
-                "-fx-border-color:       #0f4ff1;" +
-                "-fx-background-color:   #0d40c1;";
-    }
-    public String getCloseEnteredMode(){
-        return  "-fx-effect:            dropshadow(one-pass-box," +
-                                        "#828187, 2, 0.6, 1.4, 1.4);"+
-                "-fx-text-fill:         #c51300;";
     }
 
 }
