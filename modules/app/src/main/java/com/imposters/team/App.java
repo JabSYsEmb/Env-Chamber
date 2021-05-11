003053374b0c89d115fb07b4c75937bfbed96649
Cabbar Serif <abduljabbar.sha97@gmail.com>
2021-05-12 00:08:06 +0300

    make changes in style css and add  version for postDB in pom.xml

--- modules/app/src/main/java/com/imposters/team/App.java
+++ modules/app/src/main/java/com/imposters/team/App.java
@@ -28,7 +28,7 @@ public class App extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/burnIn-views/burnInTester2.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login/login.fxml"));
         primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
